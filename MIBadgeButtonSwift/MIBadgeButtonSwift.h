c840277c12fde1ff38397184a9c7dff29cbf2ffe
Andre Nguyen <andre@chainels.com>
2019-07-08 13:04:51 +0200

    create framework

---
+++ MIBadgeButtonSwift/MIBadgeButtonSwift.h
@@ -0,0 +1,19 @@
+//
+//  MIBadgeButtonSwift.h
+//  MIBadgeButtonSwift
+//
+//  Created by Andre Nguyen on 08/07/2019.
+//  Copyright © 2019 Youxel Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MIBadgeButtonSwift.
+FOUNDATION_EXPORT double MIBadgeButtonSwiftVersionNumber;
+
+//! Project version string for MIBadgeButtonSwift.
+FOUNDATION_EXPORT const unsigned char MIBadgeButtonSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MIBadgeButtonSwift/PublicHeader.h>
+
+
